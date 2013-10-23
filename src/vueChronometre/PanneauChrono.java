d512c25b9494d60fc1073f34102807b7c6e1a88c
JeromePoinas <jerome.poinas@yahoo.fr>
2013-10-23 15:09:51 +0200

    test

--- src/vueChronometre/PanneauChrono.java
+++ src/vueChronometre/PanneauChrono.java
@@ -17,7 +17,8 @@ import coucheApplicative.ModeleChronometre;
 /**
  * La classe PanneauChrono qui contiendra le chronometre.
  * 
- * @author Jérôme Poinas & Charles Neau
+ * @author Jérôme Poinas & Charles Neau !
+ * 
  * 
  */
 public class PanneauChrono extends JPanel implements ConstanteMVC, Observer,
